5bb4c7c5c39e32ca0354d603262506ac7ae17c7d
JavadAhmadi43 <99279873+javadE43@users.noreply.github.com>
2022-07-18 04:00:14 -0700

    Update app.js

--- backEnd/app.js
+++ backEnd/app.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/api', router);
+app.use('/api/v1', router);
 const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`listening on port ${port}`));
 
