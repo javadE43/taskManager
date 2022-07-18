18d0e6fe8a9bcdd01a49c5b569cd96c03fdf96f9
JavadAhmadi43 <99279873+javadE43@users.noreply.github.com>
2022-07-18 04:00:43 -0700

    Update apiConfig.js

--- frontEnd/src/api/apiConfig.js
+++ frontEnd/src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'http://localhost:4000/api/',
+    baseUrl: 'http://localhost:4000/api/v1/',
     // originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     // w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
